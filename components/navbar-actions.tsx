ef58455b4189ccafbf39c169f43ad56df86fddee
unknown <ayindeabdulrahman16@gmail.com>
2025-04-09 16:56:53 +0100

    fixed quantity indicator

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -20,10 +20,10 @@ const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button
         onClick={() => router.push("/cart")}
-        className="flex items-center rounded-full bg-transparent">
-        <ShoppingBag size={20} color="black" />
-        <div className=" absolute top-2 rounded-full  bg-red-500 p-0.5 text-sm text-white">
-          {cart.items.length}
+        className="flex relative items-center rounded-full bg-transparent">
+        <ShoppingBag size={25} color="black" />
+        <div className="absolute top-2 h-4 flex  justify-center w-4 rounded-full text-center  bg-red-500  text-sm text-white">
+          <div>{cart.items.length}</div>
         </div>
       </Button>
     </div>
